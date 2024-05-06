266705a5d56b67bde237d8aa189440cd96dd0f3e
Mykhailo_Havryliv <mkhavr.personal@gmail.com>
2024-05-06 18:20:36 +0300

    Initial User Service integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import UserService from './UserService';
 
 function App() {
+
+    const [user, setUser] = useState({ name: '', lastName: '', email: '', age: 0 });
+
+    const onChangeInput = (e) => {
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+
+    const getUser = (userId) => {
+        UserService.getUserById(userId)
+            .then(response => setUser(response.data))
+            .catch(error => console.error(`Error: ${error}`));
+    }
+
+    const createUser = (userData) => {
+        UserService.createUser(userData)
+            .then(response => setUser(response.data))
+            .catch(error => console.error(`Error: ${error}`));
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="UserPage">
+      <header className = "UserPage-header">
+        <p className = "UserPage-header-topic"> DEMOSHOP </p>
+        <p> by DevTeamLab </p>
       </header>
+        <div className="UserPage-body">
+
+            <form onSubmit={createUser}>
+                <input type="text" placeholder="First Name"
+                       name="name" value={user.name} onChange={onChangeInput}/>
+                <input type="text" placeholder="Last Name"
+                       name="lastName" value={user.lastName} onChange={onChangeInput}/>
+                <input type="email" placeholder="Email"
+                       name="email" value={user.email} onChange={onChangeInput}/>
+                <input type="number" placeholder="Age"
+                       name="age" value={user.age} onChange={onChangeInput}/>
+                <button type="submit">Create User</button>
+            </form>
+
+        </div>
     </div>
   );
 }
